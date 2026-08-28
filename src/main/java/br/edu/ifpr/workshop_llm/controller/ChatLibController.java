74dc4a205f2ffb8703b52eb959fdc8a1369414dd
Yan Lepri <20251CAS10010011@estudantes.ifpr.edu.br>
2026-08-27 21:23:29 -0300

    ref: lookup content

--- src/main/java/br/edu/ifpr/workshop_llm/controller/ChatLibController.java
+++ src/main/java/br/edu/ifpr/workshop_llm/controller/ChatLibController.java
@@ -1,13 +1,6 @@
 package br.edu.ifpr.workshop_llm.controller;
 
-import java.util.List;
-
-import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
-import org.springframework.ai.chat.memory.ChatMemory;
-import org.springframework.ai.chat.messages.Message;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,39 +13,8 @@ import br.edu.ifpr.workshop_llm.dto.ChatResponseDTO;
 @RestController
 @RequestMapping("/chat/lib")
 public class ChatLibController {
-    private ChatClient chatClient;
-
-    private ChatMemory memory;
-
-    private String CONVERSATION_ID = "42";
-
-    public ChatLibController(
-        ChatClient.Builder builder,
-        ChatMemory memory 
-    ) {
-        this.memory = memory;
-        this.chatClient = builder
-        .defaultAdvisors(
-            MessageChatMemoryAdvisor.builder(memory).build()
-        )
-        .build();
-    }
+    public ChatLibController() {}
 
     @PostMapping("")
-    public ResponseEntity<ChatResponseDTO> prompt(@RequestBody ChatRequestDTO body) {
-        String answer = chatClient.prompt()
-            .user(body.prompt())
-            .advisors(a -> a.param(ChatMemory.CONVERSATION_ID, CONVERSATION_ID))
-            .call()
-            .content();
-
-        ChatResponseDTO response = new ChatResponseDTO(answer);
-        
-        return ResponseEntity.ok(response);
-    }
-
-    @GetMapping("/history")
-    public ResponseEntity<List<Message>> getHistory() {
-        return ResponseEntity.ok(memory.get(CONVERSATION_ID));
-    }
+    public ResponseEntity<ChatResponseDTO> prompt(@RequestBody ChatRequestDTO body) {}
 }
